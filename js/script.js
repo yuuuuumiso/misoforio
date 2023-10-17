5b89cf162ffa427123913a42d9aab9674cb35855
相原勇人 <>
2023-10-17 21:19:22 +0900

    コミット

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//for ipad
+var ua = navigator.userAgent;
+var getDevice = (function() {
+if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=yes">');
+} else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+document.write('<meta name="viewport" content="width=1280, user-scalable=no, maximum-scale=1.0">');
+} else {
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=yes">');
+}
+})();
+
+
+
+
+$(function() {
+
+//vars
+var flug = true,
+    $openBtn = $('.openbtn'),
+    $body = $(document.body), 
+    $window = $(window);
+
+$window.on('scroll',function(){
+  $('.recommend .box').each(function(){
+      var imgPos = $(this).offset().top;    
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight / 3){
+        $(this).addClass('is-active');
+      } else {
+        $(this).removeClass('is-active');
+      }
+  });
+});
+
+$openBtn.click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+  $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+  $("#g-nav li").toggleClass('smooth');//li に smoothクラスを付与
+  $body.toggleClass('is-open');
+});
+ 
+});//ready
