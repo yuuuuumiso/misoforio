d53b64fe0f2ee06c2dd2f5c849b91b8f099dd929
相原勇人 <>
2023-11-08 21:41:15 +0900

    h

--- single.php
+++ single.php
@@ -10,31 +10,32 @@
 
 
 
-  <main class="content post">
+<main class="content post">
   <article class="content__main single-post">
-  <div class="single-post__inner">
-  <div class="single-post__head">
-  <?php get_template_part( 'template-parts/tpl', 'breadcrumb' ); ?>
-  <div class="single-post__info">
-  <div class="catsList catsList--single"><?php the_category(); ?></div>
-  <p class="postData"><i class="far fa-clock"></i><span><?php echo get_the_date(); ?></span><i class="fas fa-history"></i></span><?php the_modified_date(); ?></span></p>
-  </div>
-  <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-  <h1 class="postTtl01"><?php the_title(); ?></h1>
-  <p class="postThumb"><?php the_post_thumbnail(); ?></p>
-  </div>
-  <div class="postBody">
-  <?php the_content(); ?>
-  </div>
-  <?php get_template_part( 'template-parts/tpl', 'snsicon' ); ?>
-  <?php get_template_part( 'template-parts/tpl', 'pagenation' ); ?>
-  </div>
-  <?php
-endwhile;
-endif;
+    <div class="single-post__inner">
+      <div class="single-post__head">
+        <?php get_template_part('template-parts/tpl', 'breadcrumb'); ?>
+        <div class="single-post__info">
+          <div class="catsList catsList--single"><?php the_category(); ?></div>
+          <p class="postData"><i class="far fa-clock"></i><span><?php echo get_the_date(); ?></span><i class="fas fa-history"></i></span><?php the_modified_date(); ?></span></p>
+        </div>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <h1 class="postTtl01"><?php the_title(); ?></h1>
+            <p class="postThumb"><?php the_post_thumbnail(); ?></p>
+      </div>
+      <div class="postBody">
+        <?php the_content(); ?>
+      </div>
+      <?php get_template_part('template-parts/tpl', 'snsicon'); ?>
+      <?php get_template_part('template-parts/tpl', 'pagenation'); ?>
+    </div>
+<?php
+          endwhile;
+        endif;
 ?>
-</article>
-<?php get_sidebar(); ?>
+<?php comments_template(); ?>
+  </article>
+  <?php get_sidebar(); ?>
 </main>
 
 
@@ -42,4 +43,4 @@ endif;
 
 
 
-  <?php get_footer(); ?>
+<?php get_footer(); ?>
